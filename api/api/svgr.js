a79015cf2368c16fe4224223f3681a159d96e4f5
Abdullah Mzaien <xa300600@hotmail.com>
2024-11-28 15:45:03 +0300

    test: trying to fix the api to use the new function

--- api/api/svgr.js
+++ api/api/svgr.js
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const { transform } = require('@svgr/core')
+// const { transform } = require('@svgr/core')
+import { transform } from '../../packages/core/src/index';
 const jsx = require('@svgr/plugin-jsx')
 const svgo = require('@svgr/plugin-svgo')
 const prettier = require('@svgr/plugin-prettier')
